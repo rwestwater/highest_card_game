463470b9c46f2feaf84d27640c5eb45c602f3e38
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-16 13:38:02 +0100

    Card Class Completed

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -3,7 +3,17 @@ public class Card {
     private SuitType suit;
     private RankType rank;
 
+    public Card(SuitType suit, RankType rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
     public SuitType getSuit() {
         return suit;
+    }
+
+
+    public RankType getRankType() {
+        return rank;
     }
 }
